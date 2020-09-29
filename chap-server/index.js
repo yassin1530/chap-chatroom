6468dbe1efe5b55843d1a9354b765c47a0244680
yassin <yassinbousakka@web.de>
2020-09-29 16:43:26 +0200

    Yassin: Server aufgestellt, chap auth hinzugefügt und Routen für das Auth und Chatsystem erstellt

---
+++ chap-server/index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const fs = require('fs');
+const hash = require('md5');
+const dk = require('./dk/kit.js')
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+const users = dk.getUserDb;
+const auths = [];
+const chatroomUser = [];
+const chatRoomMessages = [];
+
+app.get('/', function (req, res) {
+    res.send('hello there')
+})
+
+
+app.post('/', function (req, res) {
+    let user = users.filter(val => val.username === req.body.username);
+    let isUser = user.length > 0;
+    if (req.body.username !== undefined && isUser) {
+        user = user[0];
+        let challenge = Math.floor(Math.random() * Math.floor(999999)).toString();
+        auths.push({user: user.username, hash: hash(user.password + challenge)})
+        res.send(challenge);
+    } else {
+        res.send('username not found');
+    }
+})
+
+app.post('/auth', function (req, res) {
+    console.log('request on /auth!');
+    console.log(auths)
+    let auth = auths.filter(val => val.hash === req.body.hash);
+    console.log(auth)
+    let isAuth = auth.length > 0;
+    if (isAuth) {
+        res.send('success')
+        chatroomUser.push(auth[0].user);
+    } else {
+        res.send('invalid')
+    }
+})
+
+app.post('/chat', function (req, res) {
+    chatRoom(req.body.username, chatroomUser, req.body.message)
+    res.send('validMessage');
+    console.log(chatRoomMessages)
+})
+
+app.get('/messages', function (req, res) {
+    return res.json(chatRoomMessages);    //return response as JSON
+});
+
+function chatRoom(user, chatroomList, message) {
+    const isUserAuthed = chatroomList.filter(chatroomMember => user === chatroomMember).length > 0;
+    if (isUserAuthed) {
+        chatRoomMessages.push({user: user, message: message})
+    }
+}
+
+console.log('Server Started')
+app.listen(3000)
