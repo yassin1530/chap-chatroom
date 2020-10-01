bb1f9aa543d9b57b4b9f46c0bfea4ed9ae945ee6
yassin <yassinbousakka@web.de>
2020-10-01 16:45:55 +0200

    Yassin: console.log aus kit.js entfernt

--- chap-server/dk/kit.js
+++ chap-server/dk/kit.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 function getUserDb(){
     const fs = require('fs');
     const user= JSON.parse(fs.readFileSync('./dk/user.json', 'utf8'));
-    console.log('user db: ', user);
     return user;
 }
 
