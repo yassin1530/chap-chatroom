ca96f1c56723c789fe29eaa676a7a199890c7e83
yassin <yassinbousakka@web.de>
2020-10-01 17:44:44 +0200

    Yassin: Nachrrichten vom chatraum werden erst empfangen, wenn man selbst eine nachrricht schickt, das löst den Get Call aus, da das schreiben einer nachrricht noch nicht async implementiert ist, blockiert es den intervall beim bekommen der nachrrichten

--- Client/index.js
+++ Client/index.js
@@ -16,7 +16,6 @@ async function getChallenge(url) {
 }
 
 async function postAuth(challenge, url) {
-
     let hashmd5 = hash(password + challenge);
     let res = await axios.post(url, {
         hash: hashmd5
@@ -32,12 +31,11 @@ async function postAuth(challenge, url) {
             initialMessageLoad++;
         }
 
-
         console.log('Chatroom beigetreten')
-        setInterval(function () {
-            getMessages()
-        }, 200);
-        await postChatMessage()
+            setInterval(function () {
+                getMessages()
+            }, 500);
+
     } else {
         console.log('Nutzer existiert nicht!')
     }
@@ -47,6 +45,7 @@ async function postAuth(challenge, url) {
 async function getMessages() {
     let res = await axios.get(`${globalUrl}/messages`)
     let chatArr = [];
+   // console.log('chat:  ', res.data)
     if (res.data.length > resLength) {
         for (let i = resLength; i < res.data.length; i++) {
             chatArr.push(res.data[i]);
@@ -61,17 +60,15 @@ async function getMessages() {
         }
     })
     resLength = res.data.length;
+    postChatMessage();
 }
 
 async function postChatMessage() {
     let message = readline.question(`${username}: `);
-    let chatRes = await axios.post(`${globalUrl}/chat`, {
+    await axios.post(`${globalUrl}/chat`, {
         message: message,
         username: username
     })
-    if (chatRunning === false) {
-        await postChatMessage(username);
-    }
 }
 
 getChallenge(`${globalUrl}`)
